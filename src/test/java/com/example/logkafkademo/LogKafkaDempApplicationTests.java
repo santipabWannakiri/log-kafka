16b9e51c2d4a8827b2e783f13809e610a228a133
Nokia3310 <santipab1991@mail.com>
2025-05-17 22:35:15 -0400

    Initail project

---
+++ src/test/java/com/example/logkafkademo/LogKafkaDempApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logkafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogKafkaDempApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
